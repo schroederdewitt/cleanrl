16fd1d58230be91812c15b616b262cc9cab07c3a
Christian Schroeer <schroederdewitt@gmail.com>
2023-08-26 18:41:01 +0100

    script done

--- scripts/pong_maxent_sac.sh
+++ scripts/pong_maxent_sac.sh
@@ -1,9 +1,14 @@
 wandb login # only required for the first time
-poetry run python cleanrl/sac_atari.py \
-    --seed 1 \
-    --env-id PongDeterministic-v4 \
-    --total-timesteps 3000000 \
-    --track \
-    --log-beta 8.0 \
-    --wandb-project-name mec_cleanrl \
-    --wandb-group-name pongdet-v4
+
+
+for i in 3.0 4.0 5.0 6.0 6.5 8.0
+do
+    poetry run python ../cleanrl/sac_atari.py \
+        --seed 1 \
+        --env-id PongDeterministic-v4 \
+        --total-timesteps 3000000 \
+        --track \
+        --log-beta $i \
+        --wandb-project-name mec_cleanrl \
+        --wandb-group-name pongdet-v4 &
+done
