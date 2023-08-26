50317715c4132c4435602987fa9d6b794b778c4b
Christian Schroeer <schroederdewitt@gmail.com>
2023-08-26 22:37:24 +0100

    fixed script

--- scripts/pong_maxent_sac.sh
+++ scripts/pong_maxent_sac.sh
@@ -5,10 +5,10 @@ for i in 3.0 4.0 5.0 6.0 6.5 8.0
 do
     poetry run python ../cleanrl/sac_atari.py \
         --seed 1 \
-        --env-id PongDeterministic-v4 \
+        --env-id PongNoFrameskip-v4 \
         --total-timesteps 3000000 \
         --track \
         --log-beta $i \
         --wandb-project-name mec_cleanrl \
-        --wandb-group-name pongdet-v4 &
+        --wandb-group-name pongnf-v4 &
 done
